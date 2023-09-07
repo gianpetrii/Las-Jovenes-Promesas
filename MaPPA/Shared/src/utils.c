982a76e87520ce213284c60f61f88ae6c13c3294
Nazareno Gamero <ngamero@frba.utn.edu.ar>
2023-09-07 23:54:07 +0000

    "Debuggin"

--- MaPPA/Shared/src/utils.c
+++ MaPPA/Shared/src/utils.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 
 uint8_t tiene_todas_las_configuraciones(t_config *config, char **configuraciones) {
+    printf("preFor");
     for(uint8_t i = 0; configuraciones[i] != NULL; i++) {
         printf(configuraciones[i]);
         if(!config_has_property(config, configuraciones[i]))
             return false;
     }
-
-   return true;
+    printf("postFor");
+    return true;
 }
 
